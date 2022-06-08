d7e7769dfbce0552fa1fd73ce4dd8bff0cf83ff2
gusdnr1231 <1231phw@gmail.com>
2022-06-09 00:47:57 +0900

    22.06.09
    보스 체력바, StageData 추가
    
    보스 피격 가능, 총알 일정 거리 갈 시 삭제

---
+++ Assets/01_Scripts/Boss/BossHP.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHP : MonoBehaviour
+{
+    [SerializeField] private float maxHP;
+    float curHP;
+
+    public float MaxHP => maxHP;
+    public float CurHP => curHP;
+
+    SpriteRenderer spriteRenderer;
+    
+
+    void Awake()
+    {
+        curHP = maxHP;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        Debug.Log("보스 명중");
+        curHP = curHP-dmg;
+        StopCoroutine("HitColorAnime");
+        StartCoroutine("HitColorAnime");
+        if (curHP <= 0)
+        {
+            Debug.Log("보스 사망");
+        }
+    }
+
+    IEnumerator HitColorAnime()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.3f);
+        spriteRenderer.color = Color.white;
+    }
+
+
+
+}
