46cdedf8423bf9dbaa75011a07b80d25e9bc1c70
gusdnr1231 <1231phw@gmail.com>
2022-06-17 01:57:01 +0900

    22.06.17
    레이저 패턴 추가
    
    추가할 사항
    유도탄 패턴 추가(요망)
    아마 가능할 듯함
    
    수정 사항
    레이저 패턴과 총알 발사 패턴이 겹침
    타이밍 수정 필요함
    (어떻게든 해결할 것)

--- Assets/01_Scripts/Boss/Boss.cs
+++ Assets/01_Scripts/Boss/Boss.cs
@@ -2,60 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BossState { BossStart = 0, Phase01, }
+public enum BossState { BossStart = 0, Phase1, Phase2, }
 
 public class Boss : MonoBehaviour
 {
-    private BossManager bm;
+	private BossManager bm;
 	private Movement movement;
 	private BossState state = BossState.BossStart;
 	private float bossPoint = 5f;
-
+	bool lazer;
 
 	private void Awake()
 	{
 		bm = GetComponent<BossManager>();
 		movement = GetComponent<Movement>();
 	}
-
-	private void Update()
+	private void Start()
 	{
 		StartCoroutine("BossStart");
 	}
-
-
-	public void ChangeState(BossState newsState)
-	{
-		StopCoroutine(state.ToString());
-		state = newsState;
-		StartCoroutine(state.ToString());
-	}
-
 	private IEnumerator BossStart()
 	{
 		movement.MoveTo(Vector3.down);
 		while (true)
 		{
-			if ( transform.position.y <= bossPoint)
+			if (transform.position.y <= bossPoint)
 			{
 				movement.MoveTo(Vector3.zero);
-				ChangeState(BossState.Phase01);
+				ChangeState(BossState.Phase1);
 			}
 			yield return null;
 		}
 	}
 
-	private IEnumerator Phase01()
+	public void ChangeState(BossState newState)
 	{
-		bm.StartFiring(AttackType.CircleFiring);
-		while (true)
-		{
-			yield return null;
-		}
+		StopCoroutine(state.ToString());
+		state = newState;
+		StartCoroutine(state.ToString());
 	}
 
 
+	private IEnumerator Phase1()
+	{
+		bm.StartFiring(AttackType.CircleFiring);
+		yield return new WaitForSeconds(3f);
+		bm.StopFiring(AttackType.CircleFiring);
+		ChangeState(BossState.Phase2);
+	}
 
+	private IEnumerator Phase2()
+	{
+		bm.StartFiring(AttackType.LazerFiring);
+		yield return new WaitForSeconds(3f);
+		bm.StopFiring(AttackType.CircleFiring);
+		ChangeState(BossState.Phase1);
 
-
+	}
 }
--- Assets/01_Scripts/Boss/BossManager.cs
+++ Assets/01_Scripts/Boss/BossManager.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AttackType { CircleFiring = 0, }
+public enum AttackType { CircleFiring = 0, LazerFiring, }
 
 
 public class BossManager : MonoBehaviour
 {
-	[SerializeField] private GameObject bullet;
+	[SerializeField] private GameObject bullet; //패턴 1 총알
+	[SerializeField] private GameObject LazerRange; //패턴 2 레이저 범위
+	[SerializeField] private GameObject Lazer; //패턴 2 레이저
+
     BossMove bossMove;
-	
+
 	//패턴에 쓰이는 변수
 	float attackRate = 0;
 	int count = 0;
@@ -33,7 +36,7 @@ public class BossManager : MonoBehaviour
 
 	private IEnumerator CircleFiring()
 	{
-		attackRate = 10f;
+		attackRate = 0.5f;
 		count = 20;
 		intervalAngle = 360/count;
 		weightAngle = 0;
@@ -51,9 +54,27 @@ public class BossManager : MonoBehaviour
 			weightAngle += 1;
 			yield return new WaitForSeconds(attackRate);
 		}
-
-
 	}
+
+	private IEnumerator LazerFiring()
+	{
+		attackRate = 3f;
+		count = 4;
+		while (true)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				float y = Random.Range(-4f, 2f);
+				GameObject lr /* 레이저 범위 복사 */ = Instantiate(LazerRange, new Vector3(0, y, 0), Quaternion.identity);
+				yield return new WaitForSeconds(0.4f);
+				Destroy(lr);
+				GameObject lzr = Instantiate(Lazer, new Vector3(0, y, 0), Quaternion.identity);
+				Destroy(lzr, 0.5f);
+			}
+			yield return new WaitForSeconds(attackRate);
+		}
+	}
+
 
 
 }
